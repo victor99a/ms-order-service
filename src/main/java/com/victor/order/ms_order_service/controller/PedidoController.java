d665e757469f73ad662fcb02306da6b7b17aba9b
victor99a <vi.barreraj@duocuc.cl>
2025-11-21 00:56:45 -0300

    se agrega puerto para front

--- src/main/java/com/victor/order/ms_order_service/controller/PedidoController.java
+++ src/main/java/com/victor/order/ms_order_service/controller/PedidoController.java
@@ -15,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/pedidos")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class PedidoController {
     private final PedidoService pedidoService;
 
